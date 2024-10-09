67359d2768630c178f693328b1fd1f7281634adb
bengal21 <87319796+bengal21@users.noreply.github.com>
2024-10-09 22:05:55 +0600

    Add files via upload

--- default.py
+++ default.py
@@ -1,6 +1,7 @@
-import pygame # подключаем библиотеку
-import sys # подключаем модуль для 
-from random import *
+import pygame
+import sys
+import os
+from random import randint
 
 pygame.init()
 
@@ -9,65 +10,43 @@ HEIGHT = 500
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
-player = pygame.Rect(400, 400, 50, 50)
-player_img_orig = pygame.image.load('steve.png')
-player_img = pygame.transform.scale(player_img_orig, (player.width, player.height))
-enemy = pygame.Rect(50, 50, 30, 30)
-enemy_img = pygame.image.load('creeper.png')
-enemy_img = pygame.transform.scale(enemy_img, (enemy.width, enemy.height))
+click = pygame.Rect(225, 225, 50, 50)
+score = 0
+seconds_left = 60
+iterations = 0
 
-direction = 'none'
-game_state = 0
+font = pygame.font.SysFont('Arial', 20)
+left_seconds_text = font.render(str(seconds_left), False, (255, 0, 0))
+score_text = font.render(str(score), False, (255, 0, 0))
+
+
 
 while True:
     screen.fill((255, 255, 255))
-    if game_state == 0:
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                sys.exit()
-            if e.type == pygame.KEYDOWN:
-                if e.key == pygame.K_SPACE:
-                    game_state = 1
-    if game_state == 1:
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                sys.exit()
-            if e.type == pygame.KEYDOWN:
-                if e.key == pygame.K_RIGHT:
-                    direction = 'right'
-                elif e.key == pygame.K_LEFT:
-                    direction = 'left'
-                elif e.key == pygame.K_UP:
-                    direction = 'up'
-                elif e.key == pygame.K_DOWN:
-                    direction = 'down'
-            if e.type == pygame.KEYUP:
-                direction = 'none'
-
-        if direction == 'right':
-            player.x += 5
-        elif direction == 'left':
-            player.x -= 5
-        elif direction == 'up':
-            player.y -= 5
-        elif direction == 'down':
-            player.y += 5
-
-
-        if player.colliderect(enemy):
-            enemy.x = randint(0, 470)
-            enemy.y = randint(0, 470)
-            player.width += 5
-            player.height += 5
-            player_img = pygame.transform.scale(player_img_orig, (player.width, player.height))
-
-
-        if player.width >= 100:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
             sys.exit()
-        # Здесь весь движ
-        screen.blit(player_img, player)
-        screen.blit(enemy_img, enemy)
+        if e.type == pygame.MOUSEBUTTONDOWN:
+            if click.collidepoint(e.pos[0], e.pos[1]):
+                score += 1
+                score_text = font.render(str(score), False, (255, 0, 0))
+                click.x = randint(0, 450)
+                click.y = randint(0, 450)
+
+
+    pygame.draw.rect(screen, (0, 0, 0), click)
+    screen.blit(left_seconds_text, (10, 10))
+    screen.blit(score_text, (480, 10))
+
+    iterations += 1
+    if iterations == 60:
+        seconds_left -= 1
+        iterations = 0
+        left_seconds_text = font.render(str(seconds_left), False, (255, 0, 0))
+
+    if seconds_left == -1:
+        sys.exit()
+
     pygame.display.update()
     clock.tick(60)
-    
 
